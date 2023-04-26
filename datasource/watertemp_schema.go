4ea632381e7e8d2cbdc7e21c50eb4e4ea329af2b
fredrikwangberg <fredrik.wangberg@meltwater.com>
2023-04-26 20:55:17 +0200

    Fix wrongly named 'FormattedTime' in WaterTemperatureSchema

--- datasource/watertemp_schema.go
+++ datasource/watertemp_schema.go
@@ -1,12 +1,12 @@
 package datasource
 
 type WaterTemperatureSchema struct {
-	Type          string
-	Temp_water    float32
-	FormattedTime string
-	Alias         string
-	Ts            int
-	Latitude      float32
-	Longitude     float32
-	Gmap          string
+	Type           string
+	Temp_water     float32
+	Formatted_Time string
+	Alias          string
+	Ts             int
+	Latitude       float32
+	Longitude      float32
+	Gmap           string
 }
--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"github.com/fredrikwangberg/watertemp/datasource"
 )
 
-
 func main() {
 	data, err := getData(false)
-
 	if err == nil {
 		coldSite := datasource.GetLocationWithLowestTemperature(data)
-	    fmt.Println("Location with lowest water temperature:")
+		fmt.Println("Location with lowest water temperature:")
 		fmt.Printf(datasource.WaterTemperatureSchemaToString(coldSite))
 	} else {
 		fmt.Println("No data, err?")
@@ -21,7 +19,7 @@ func main() {
 
 func getData(useMocked bool) ([]datasource.WaterTemperatureSchema, error) {
 	if useMocked {
-	    fmt.Println("Using mocked data")
+		fmt.Println("Using mocked data")
 		return datasource.GetMockedData()
 	} else {
 		apiUrl := "https://sodertaljeglue.eu-gb.mybluemix.net/getwatertemp"
