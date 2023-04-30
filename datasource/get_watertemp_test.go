6bac747d5b3becd505ab854b116e7169b0d2ded5
fredrikwangberg <fredrik.wangberg@meltwater.com>
2023-04-30 20:30:25 +0200

    a failing test

--- datasource/get_watertemp_test.go
+++ datasource/get_watertemp_test.go
@@ -1,10 +1,9 @@
 package datasource
 
 import (
-	"testing"
 	"github.com/skyportsystems/testify/assert"
+	"testing"
 )
-
 
 // TODO, write tests on parseing of json
 //   Malformed json
@@ -13,17 +12,17 @@ import (
 func TestParsing(t *testing.T) {
 	testJson := `[{"type":"Watertemp","temp_water":7.8,"formatted_time":"Nov 02 2021 20:10:14","alias":"Site1","ts":1635880214580,"latitude":59.16883,"longitude":17.59184,"gmap":"link"}]`
 	data, _ := parseResponseBody([]byte(testJson))
-	assert.Equal(t, data[0].Alias, "Site1")
+	assert.Equal(t, data[0].Alias, "Site2")
 }
 
-func TestCompareLocationTemperatures(t *testing.T){
+func TestCompareLocationTemperatures(t *testing.T) {
 	location1 := WaterTemperatureSchema{
-		Temp_water:    6,
-		Alias:         "Warmest",
+		Temp_water: 6,
+		Alias:      "Warmest",
 	}
 	location2 := WaterTemperatureSchema{
-		Temp_water:    5,
-		Alias:         "Warmest",
+		Temp_water: 5,
+		Alias:      "Warmest",
 	}
 	if hasLowerTemperature(location1, location2) {
 		t.Fatalf("location2 has lowest temperature, not location1")
